8d397e94ae55c0d2ed63a235079f94efca02e02c
smxrlxp <sergiomartinlopez97@gmail.com>
2019-05-31 09:18:04 +0200

    WebPresentacion

--- assets/js/arriba.js
+++ assets/js/arriba.js
@@ -1,27 +1,17 @@
-<script type="text/javascript">/*<![CDATA[*/
-jQuery(document).ready(function() {
-    var duration = 900;
-    $(".back-to-top").hide();
- 
-    $(".back-to-top").on("click", function(e) {
-        e.preventDefault();
-        toTop();
+$(document).ready(function() {
+    $('.up').hide();
+    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100){
+            $('.up').fadeIn('1000');
+        } else{
+            $('.up').fadeOut('1000');
+        }
     });
- 
-    $(function toTop() {
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > 100) {
-                $(".back-to-top").fadeIn();
-            } else {
-                $(".back-to-top").fadeOut();
-            }
-        });
-        $(".back-to-top").click(function() {
-            $("body,html").animate({
-                scrollTop: 0
-            }, duration);
-            return false;
-        });
+
+    $('.up').click(function(){
+        $('body, html').animate({
+            scrollTop: 0
+        },150);
     });
 });
-/*]]>*/</script>
